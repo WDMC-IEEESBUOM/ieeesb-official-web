2566b60f096a236a1888e9f246ef613be073311c
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-01-13 12:15:37 +0530

    added midnight color, created layout

--- app/(home)/eventTimelineSection/eventTimelineSection.tsx
+++ app/(home)/eventTimelineSection/eventTimelineSection.tsx
@@ -3,163 +3,31 @@ import Image from "next/image";
 
 const EventTimelineSection = () => {
   return (
-    <span className="flex flex-col items-center px-5">
-      <div className="text-neutral-400 text-center text-6xl font-semibold whitespace-nowrap max-md:text-4xl">
-        EVENTS
+    <div className="container bg-midnight lg:w-[1360px] lg:h-[1438px] rounded-[40px] block">
+
+      <p className="block text-center text-cyan-400 text-[60px] my-10">EVENTS</p>
+
+      <div className="container flex">
+        <div className="mx-auto gap-9 column-3 w-[475px] content-start">
+
+          <div className="w-full rounded-[50px] h-[200px] my-20 bg-white grid justify-items-end">
+            <Image className="w-[200px] h-[200px] bg-black rounded-r-[50px]" src="/eventbg.jpg" width={200} height={200} alt="" />
+          </div>
+          <div className="w-full rounded-[50px] h-[200px] my-20 bg-white"></div>
+          <div className="w-full rounded-[50px] h-[200px] my-20 bg-white"></div>
+
+        </div>
+
+        <div className="mx-auto column-1">2</div>
+
+        <div className="mx-auto column-3 w-[475px] content-start ">
+          <div className="w-full rounded-[50px] h-[200px] my-20 bg-white"></div>
+          <div className="w-full rounded-[50px] h-[200px] my-20 bg-white"></div>
+          <div className="w-full rounded-[50px] h-[200px] my-20 bg-white"></div>
+        </div>
       </div>
-      <div className="self-stretch w-full mt-48 max-md:max-w-full max-md:mt-10">
-        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-          <div className="flex flex-col items-stretch w-[46.2%] max-md:w-full max-md:ml-0">
             
-          
-
-          <div className="bg-white pl-10 rounded-[50px] max-md:max-w-full max-md:pl-5 mt-5 mb-5">
-      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <div className="flex flex-col items-stretch w-[53%] max-md:w-full max-md:ml-0">
-          <header className="flex flex-col items-stretch my-auto max-md:mt-10">
-            <h3 className="text-neutral-700 text-3xl font-semibold">2020</h3>
-            <h2 className="text-neutral-700 text-center text-4xl font-light whitespace-nowrap mt-9">Event name</h2>
-          </header>
-        </div>
-        <div className="flex flex-col items-stretch w-[47%] ml-5 max-md:w-full max-md:ml-0">
-    
-          <Image
-            className="flex flex-col mx-auto rounded-r-[50px] max-md:mt-10"
-            src="/eventbg.jpg"
-            width={191}
-            height={160}
-            alt="Event image"
-        />
-        </div>
-      </div>
     </div>
-
-    <div className="bg-white pl-10 rounded-[50px] max-md:max-w-full max-md:pl-5 mt-5 mb-5">
-      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <div className="flex flex-col items-stretch w-[53%] max-md:w-full max-md:ml-0">
-          <header className="flex flex-col items-stretch my-auto max-md:mt-10">
-            <h3 className="text-neutral-700  text-3xl font-semibold">2020</h3>
-            <h2 className="text-neutral-700 text-center text-4xl font-light whitespace-nowrap mt-9">Event name</h2>
-          </header>
-        </div>
-        <div className="flex flex-col items-stretch w-[47%] ml-5 max-md:w-full max-md:ml-0">
-        <Image
-            className="flex flex-col mx-auto rounded-r-[50px] max-md:mt-10"
-            src="/eventbg.jpg"
-            width={191}
-            height={160}
-            alt="Event image"
-        />
-        </div>
-      </div>
-    </div>
-
-    <div className="bg-white pl-10 rounded-[50px] max-md:max-w-full max-md:pl-5 mt-5 mb-5">
-      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <div className="flex flex-col items-stretch w-[53%] max-md:w-full max-md:ml-0">
-          <header className="flex flex-col items-stretch my-auto max-md:mt-10">
-            <h3 className="text-neutral-700 text-3xl font-semibold">2020</h3>
-            <h2 className="text-neutral-700 text-center text-4xl font-light whitespace-nowrap mt-9">Event name</h2>
-          </header>
-        </div>
-        <div className="flex flex-col items-stretch w-[47%] ml-5 max-md:w-full max-md:ml-0">
-        <Image
-            className="flex flex-col mx-auto rounded-r-[50px] max-md:mt-10"
-            src="/eventbg.jpg"
-            width={191}
-            height={160}
-            alt="Event image"
-        />
-        </div>
-      </div>
-    </div>
-    
-
-
-          </div>
-          <div className="flex flex-col items-stretch w-[45%] ml-5 max-md:w-full max-md:ml-0">
-            
-            
-
-          <div className="bg-white pr-7 rounded-[50px] max-md:max-w-full max-md:pr-5 mt-5 mb-5">
-      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <header className="flex flex-col items-stretch w-[48%] max-md:w-full max-md:ml-0">
-       
-        <Image
-            className="flex shrink-0 flex-col mx-auto rounded-l-[50px] max-md:mt-10"
-            src="/eventbg.jpg"
-            width={191}
-            height={160}
-            alt="Event image"
-        />
-        </header>
-        <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
-          <div className="flex flex-col mt-9 items-end max-md:mt-10">
-            <div className="text-neutral-700 text-center text-3xl font-semibold whitespace-nowrap">
-              2020
-            </div>
-            <div className="text-neutral-700 text-center text-4xl font-light self-stretch whitespace-nowrap mt-9">
-              Event name
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="bg-white pr-7 rounded-[50px] max-md:max-w-full max-md:pr-5 mt-5 mb-5">
-      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <header className="flex flex-col items-stretch w-[48%] max-md:w-full max-md:ml-0">
-        <Image
-            className=" flex shrink-0 flex-col mx-auto rounded-l-[50px] max-md:mt-10"
-            src="/eventbg.jpg"
-            width={191}
-            height={160}
-            alt="Event image"
-        />
-        </header>
-        <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
-          <div className="flex flex-col mt-9 items-end max-md:mt-10">
-            <div className="text-neutral-700 text-center text-3xl font-semibold whitespace-nowrap">
-              2020
-            </div>
-            <div className="text-neutral-700 text-center text-4xl font-light self-stretch whitespace-nowrap mt-9">
-              Event name
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="bg-white pr-7 rounded-[50px] max-md:max-w-full max-md:pr-5 mt-5 mb-5">
-      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <header className="flex flex-col items-stretch w-[48%] max-md:w-full max-md:ml-0">
-        <Image
-            className=" flex shrink-0 flex-col mx-auto rounded-l-[50px] max-md:mt-10"
-            src="/eventbg.jpg"
-            width={191}
-            height={160}
-            alt="Event image"
-        />
-        </header>
-        <div className="flex flex-col items-stretch w-[52%] ml-5 max-md:w-full max-md:ml-0">
-          <div className="flex flex-col mt-9 items-end max-md:mt-10">
-            <div className="text-neutral-700 text-center text-3xl font-semibold whitespace-nowrap">
-              2020
-            </div>
-            <div className="text-neutral-700 text-center text-4xl font-light self-stretch whitespace-nowrap mt-9">
-              Event name
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-            
-          </div>
-        </div>
-      </div>
-    </span>
   );
 }
 
