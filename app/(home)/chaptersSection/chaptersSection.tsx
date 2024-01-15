9783d5535c2865e59e51c4a76be32c1de0821f8b
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2024-01-15 20:56:20 +0530

    style(86endbv8k): updated styles according to the new UI
    Updated logos, colors & text alignment according to the new UI

--- app/(home)/chaptersSection/chaptersSection.tsx
+++ app/(home)/chaptersSection/chaptersSection.tsx
@@ -6,7 +6,7 @@ import WIE from "./images/WIE.png";
 import EMB from "./images/EMB.png";
 import NPSS from "./images/NPSS.png";
 import IES from "./images/IES.png";
-import SPP from "./images/SPP.png";
+import SPS from "./images/SPS.png";
 import COMSOC from "./images/comsoc.png";
 import PELS from "./images/PELS.png";
 import IAS from "./images/IAS.png";
@@ -15,12 +15,12 @@ import PROCOM from "./images/procom.png";
 
 const chaptersSection = () => {
   const images = [
-    RAS, PES, WIE, EMB, NPSS, IES, SPP, COMSOC, PELS, IAS, CS, PROCOM,
+    RAS, PES, WIE, EMB, NPSS, IES, SPS, COMSOC, PELS, IAS, CS, PROCOM,
   ];
 
   return (
     <div
-      className="relative overflow-hidden rounded-3xl bg-gradient-to-r from-red-50 to-blue-200 p-8 w-screen"
+      className="relative overflow-hidden rounded-3xl bg-white p-8 w-screen"
       style={{
         width: "fit-content",
         height: "fit-content",
@@ -29,23 +29,25 @@ const chaptersSection = () => {
       }}
     >
       <div className="p-4"> 
-        <h2 className="text-5xl font-bold text-left mb-4 text-gray-700 pb-5">
+        <h2 className="text-5xl font-bold text-center mb-4 text-gray-700 pb-5">
           CHAPTERS
         </h2>
    
         <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-          {images.map((imagePath, index) => (
-            <Image
-              key={index}
-              src={imagePath}
-              alt={`Image from ${imagePath}`}
-              width={300}
-              height={300}
-              objectFit="cover"
-              className="rounded-lg shadow-md"
-            />
-          ))}
-        </div>
+  {images.map((imagePath, index) => (
+    <div key={index} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Image
+        src={imagePath}
+        alt={`Image from ${imagePath}`}
+        width={300}
+        height={300}
+        objectFit="cover"
+        className="rounded-lg"
+      />
+    </div>
+  ))}
+</div>
+
       </div>
     </div>
    );
