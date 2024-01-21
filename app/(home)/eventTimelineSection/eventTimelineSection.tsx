a8ef04a6e846e0a634d2050f1cf1e059a3f62a28
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-01-21 15:40:00 +0530

    fix(responsiveness): added new breakpoints for the design

--- app/(home)/eventTimelineSection/eventTimelineSection.tsx
+++ app/(home)/eventTimelineSection/eventTimelineSection.tsx
@@ -3,74 +3,74 @@ import Image from "next/image";
 
 const EventTimelineSection = () => {
   return (
-    <div className="container rounded-[20px] w-[375px] h-[100%] block bg-midnight lg:w-[95%] lg:h-[100%] lg:rounded-[40px]">
+    <div className="container rounded-[20px] w-[375px] h-[100%] block bg-midnight lg:w-[100%] lg:h-[100%] lg:rounded-[40px]">
 
-      <p className="block text-[30px] my-5 text-center text-cyan-400 sm:text-[60px] sm:my-10">EVENTS</p>
+      <p className="block text-[30px] my-5 text-center text-cyan-400 lg:text-[60px] lg:my-10">EVENTS</p>
 
-      <div className="container block sm:flex ">
-        <div className="mx-auto gap-9 content-start column-3 w-[90%] sm:w-[475px] sm:h-[100%] ">
+      <div className="container block lg:flex ">
+        <div className="mx-auto gap-9 content-start column-3 w-[90%] lg:w-[475px] lg:h-[100%] ">
 
-          <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] sm:rounded-[50px] sm:h-[200px] sm:mb-[200px]">
+          <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] lg:rounded-[50px] lg:h-[200px] lg:mb-[200px] lg:w-[90%] lg:mx-5">
             <div className="w-1/2 grid place-items-center">
               <p className="text-lg font-large">MoraForesight 1.0</p>
               <p className="text-md">2023</p>
               </div>
-            <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] sm:rounded-r-[50px] sm:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
+            <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] lg:rounded-r-[50px] lg:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
           </div>
-          <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] sm:rounded-[50px] sm:h-[200px] sm:my-[200px]">
+          <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] lg:rounded-[50px] lg:h-[200px] lg:my-[200px] lg:w-[90%] lg:mx-5">
             <div className="w-1/2 grid place-items-center">
               <p className="text-lg font-large">MoraForesight 1.0</p>
               <p className="text-md">2023</p>
               </div>
-            <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] sm:rounded-r-[50px] sm:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
+            <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] lg:rounded-r-[50px] lg:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
           </div>
-          <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] sm:rounded-[50px] sm:h-[200px] sm:my-[200px]">
+          <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] lg:rounded-[50px] lg:h-[200px] lg:my-[200px] lg:w-[90%] lg:mx-5">
             <div className="w-1/2 grid place-items-center">
               <p className="text-lg font-large">MoraForesight 1.0</p>
               <p className="text-md">2023</p>
               </div>
-            <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] sm:rounded-r-[50px] sm:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
+            <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] lg:rounded-r-[50px] lg:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
           </div>
 
         </div>
 
-        <div className="sm:flex">
-          <div className="sm:mx-auto sm:rounded-[50px] sm:column-1 sm:w-2 sm:h-[1350px] sm:bg-cyan-lite"></div>
+        <div className="lg:flex">
+          <div className="lg:mx-auto lg:rounded-[50px] lg:column-1 lg:w-2 lg:h-[1350px] lg:bg-cyan-lite"></div>
           
-          <div className="sm:grid sm:grid-rows-6 gap-0">
-            <div className="sm:-ml-6 sm:z-20 sm:mt-[120px] sm:rounded-full sm:h-10 sm:w-10 sm:flex sm:items-center sm:bg-cyan-lite"></div>
-            <div className="sm:-ml-6 sm:z-20 sm:mt-[90px] sm:rounded-full sm:h-10 sm:w-10 sm:flex sm:items-center sm:bg-cyan-lite"></div> 
-            <div className="sm:-ml-6 sm:z-20 sm:mt-[50px] sm:rounded-full sm:h-10 sm:w-10 sm:flex sm:items-center sm:bg-cyan-lite"></div> 
-            <div className="sm:-ml-6 sm:z-20 sm:mt-[20px] sm:rounded-full sm:h-10 sm:w-10 sm:flex sm:items-center sm:bg-cyan-lite"></div> 
-            <div className="sm:-ml-6 sm:z-20 sm:-mt-[35px] sm:rounded-full sm:h-10 sm:w-10 sm:flex sm:items-center sm:bg-cyan-lite"></div> 
-            <div className="sm:-ml-6 sm:z-20 sm:-mt-[65px] sm:rounded-full sm:h-10 sm:w-10 sm:flex sm:items-center sm:bg-cyan-lite"></div>  
+          <div className="lg:grid lg:grid-rows-6 gap-0">
+            <div className="lg:-ml-6 lg:z-20 lg:mt-[120px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:items-center lg:bg-cyan-lite"></div>
+            <div className="lg:-ml-6 lg:z-20 lg:mt-[90px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:items-center lg:bg-cyan-lite"></div> 
+            <div className="lg:-ml-6 lg:z-20 lg:mt-[50px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:items-center lg:bg-cyan-lite"></div> 
+            <div className="lg:-ml-6 lg:z-20 lg:mt-[20px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:items-center lg:bg-cyan-lite"></div> 
+            <div className="lg:-ml-6 lg:z-20 lg:-mt-[35px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:items-center lg:bg-cyan-lite"></div> 
+            <div className="lg:-ml-6 lg:z-20 lg:-mt-[65px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:items-center lg:bg-cyan-lite"></div>  
           </div>
         </div>
         
 
-        <div className="mx-auto gap-9 content-start column-3 w-[90%] sm:w-[475px] sm:h-[100%] pt-[40px]">
-          <div className="w-full h-[150px]  bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] sm:rounded-[50px] sm:h-[200px] sm:my-[200px]">
+        <div className="mx-auto gap-9 content-start column-3 w-[90%] lg:w-[475px] lg:h-[100%] pt-[40px]">
+          <div className="w-full h-[150px]  bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] lg:rounded-[50px] lg:h-[200px] lg:my-[200px] lg:w-[90%] lg:mx-5">
               <div className="w-1/2 grid place-items-center">
                 <p className="text-lg font-large">MoraForesight 1.0</p>
                 <p className="text-md">2023</p>
                 </div>
-              <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] sm:rounded-r-[50px] sm:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
+              <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] lg:rounded-r-[50px] lg:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
             </div>
 
-            <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] sm:rounded-[50px] sm:h-[200px] sm:my-[200px]">
+            <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] lg:rounded-[50px] lg:h-[200px] lg:my-[200px] lg:w-[90%] lg:mx-5">
               <div className="w-1/2 grid place-items-center">
                 <p className="text-lg font-large">MoraForesight 1.0</p>
                 <p className="text-md">2023</p>
                 </div>
-              <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] sm:rounded-r-[50px] sm:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
+              <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] lg:rounded-r-[50px] lg:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
             </div>
 
-            <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] sm:rounded-[50px] sm:h-[200px] sm:my-[200px]">
+            <div className="w-full h-[150px] bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 my-10 flex rounded-[20px] lg:rounded-[50px] lg:h-[200px] lg:my-[200px] lg:w-[90%] lg:mx-5">
               <div className="w-1/2 grid place-items-center">
                 <p className="text-lg font-large">MoraForesight 1.0</p>
                 <p className="text-md">2023</p>
                 </div>
-              <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] sm:rounded-r-[50px] sm:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
+              <div className="w-1/2 grid justify-items-end"><Image className="w-[200px] h-[148px] bg-black rounded-r-[20px] lg:rounded-r-[50px] lg:h-[198px]" src="/eventbg.jpg" width={200} height={200} alt="" /></div>
             </div>
         </div>
       </div>
