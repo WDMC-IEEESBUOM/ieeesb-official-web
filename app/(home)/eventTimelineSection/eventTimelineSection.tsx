f665bc14af6f170eed3f2978b722d3a1f3da9a51
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2024-01-25 18:54:24 +0530

    feat(event-timeline):added gradient to header

--- app/(home)/eventTimelineSection/eventTimelineSection.tsx
+++ app/(home)/eventTimelineSection/eventTimelineSection.tsx
@@ -182,7 +182,7 @@ const EventTimelineSection = () => {
   return (
     <div className="container rounded-[20px] w-[375px] h-[100%] block bg-midnight lg:w-[100%] lg:h-[100%] lg:rounded-[40px]">
 
-      <p className="block text-[30px] my-5 text-center text-cyan-400 lg:text-[60px] lg:my-10">EVENTS</p>
+      <p className="block text-[30px] my-5 text-center bg-gradient-to-r from-[#007198] to-[#f1C4EF] text-transparent bg-clip-text lg:text-[60px] lg:my-10">EVENTS</p>
 
       <div className="container block lg:flex ">
         <div className="mx-auto gap-9 content-start column-3 w-[90%] lg:w-[475px] lg:h-[100%] ">
@@ -222,17 +222,17 @@ const EventTimelineSection = () => {
         </div>
 
         <div className="lg:flex">
-          <div className="lg:mx-auto lg:rounded-[50px] lg:column-1 lg:w-2 lg:h-[1700px] lg:bg-cyan-lite"></div>
+          <div className="lg:mx-auto lg:rounded-[50px] lg:column-1 lg:w-2 lg:h-[1700px] lg:bg-gradient-to-b from-[#007198] to-[#146dc6]"></div>
           
           <div className="lg:block">
-            <div className="lg:-ml-6 lg:z-20 lg:mt-[120px] lg:rounded-full lg:h-10 lg:w-10 lg:flex  lg:bg-cyan-lite"></div>
-            <div className="lg:-ml-6 lg:z-20 lg:mt-[170px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div> 
-            <div className="lg:-ml-6 lg:z-20 lg:mt-[160px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div> 
-            <div className="lg:-ml-6 lg:z-20 lg:mt-[155px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div> 
-            <div className="lg:-ml-6 lg:z-20 lg:mt-[155px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div> 
-            <div className="lg:-ml-6 lg:z-20 lg:mt-[155px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div>  
-            <div className="lg:-ml-6 lg:z-20 lg:mt-[155px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div>  
-            <div className="lg:-ml-6 lg:z-20 lg:mt-[160px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div>  
+            <div className="lg:event1 lg:-ml-6 lg:z-20 lg:mt-[120px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div>
+            <div className="lg:event2 lg:-ml-6 lg:z-20 lg:mt-[170px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div> 
+            <div className="lg:event3 lg:-ml-6 lg:z-20 lg:mt-[160px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div> 
+            <div className="lg:event4 lg:-ml-6 lg:z-20 lg:mt-[155px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div> 
+            <div className="lg:event5 lg:-ml-6 lg:z-20 lg:mt-[155px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div> 
+            <div className="lg:event6 lg:-ml-6 lg:z-20 lg:mt-[155px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div>  
+            <div className="lg:event7 lg:-ml-6 lg:z-20 lg:mt-[155px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div>  
+            <div className="lg:event8 lg:-ml-6 lg:z-20 lg:mt-[160px] lg:rounded-full lg:h-10 lg:w-10 lg:flex lg:bg-cyan-lite"></div>  
           </div>
         </div>
         
