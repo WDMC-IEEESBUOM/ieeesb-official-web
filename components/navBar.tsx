7f2224cb45133b5785060af7eccd7f497de58701
chamodperera <chamodperera87@gmail.com>
2024-01-26 12:31:21 +0530

    fix(navbar): changed bg and added hover animations

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -4,22 +4,24 @@ import brand from "../public/IEEE_logo.png";
 
 const NavBar = () => {
   return (
-    <div className={styles.navbar}>
-      <span className={styles.brand_container}>
-        <Image
-          src={brand}
-          className={styles.brand}
-          alt="IEEE Logo"
-          width={400}
-          height={400}
-          quality={100}
-        />
-      </span>
-      <ul className={styles.nav_container}>
-        <li className={styles.nav}>Home</li>
-        <li className={styles.nav}>Team</li>
-        <li className={styles.nav}>Register</li>
-      </ul>
+    <div className={styles.navbar_container}>
+      <div className={styles.navbar}>
+        <span className={styles.brand_container}>
+          <Image
+            src={brand}
+            className={styles.brand}
+            alt="IEEE Logo"
+            width={400}
+            height={400}
+            quality={100}
+          />
+        </span>
+        <ul className={styles.nav_container}>
+          <li className={styles.nav}>Home</li>
+          <li className={styles.nav}>Team</li>
+          <li className={styles.nav}>Register</li>
+        </ul>
+      </div>
     </div>
   );
 };
