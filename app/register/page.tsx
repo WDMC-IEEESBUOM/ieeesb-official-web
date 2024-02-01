5be9b997886f6f0959053346a172f5bdbea205f4
Kalhara JA <kalharajak.21@uom.lk>
2024-02-01 16:13:19 +0530

    fix: fix issues

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,7 @@ const RegisterPage = () => {
     return (
         <main className="flex min-h-screen flex-col items-center bg-white justify-between scroll-smooth" >
             <div className="w-[100%] bg-black opacity-95 rounded-b-[40px] h-[15px]"></div>
-            <iframe src="https://sbregformieee.vercel.app/" className="w-full lg:h-[1150px] md:h-[1050px] h-[1150px] " >
+            <iframe src="https://sbregformieee.vercel.app/" className="w-full lg:h-[1250px] md:h-[1250px] h-[1250px] " >
             </iframe>
         </main>
     );
