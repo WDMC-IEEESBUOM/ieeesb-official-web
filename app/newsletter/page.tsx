aa625b2bea662835994c5d379107d8162c8d06c0
Kalhara JA <kalhara.ja@gmail.com>
2024-02-26 19:03:37 +0530

    fix: flipping issues

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -1,11 +1,11 @@
 'use client';
-import React, { useRef, useState, useCallback } from 'react';
+import React, { useRef, useState, useCallback, useEffect } from 'react';
 import HTMLFlipBook from 'react-pageflip';
 import Image from 'next/image';
 
 // Importing images dynamically
 const images = Array.from({ length: 27 }, (_, index) => {
-  return require(`../../public/edition1/IEEE SB UOM Newsletter_page-${String(index + 1).padStart(4, '0')}.jpg`);
+  return require(`../../public/newsletter/edition1/${String(index + 1)}.jpg`);
 });
 
 interface MyBookProps { }
@@ -13,10 +13,11 @@ interface MyBookProps { }
 const MyBook: React.FC<MyBookProps> = () => {
   const [currentPage, setCurrentPage] = useState<number>(0);
   const pageFlipRef = useRef<typeof HTMLFlipBook>(null);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
-  const isMobile = () => {
-    return window.innerWidth < 780;
-  };
+  useEffect(() => {
+    setIsMobile(window.innerWidth < 768);
+  }, []);
 
   return (
     <main className="flex min-h-full flex-col items-center bg-white justify-between scroll-smooth overflow-hidden">
@@ -26,20 +27,20 @@ const MyBook: React.FC<MyBookProps> = () => {
           INNOSPHERE - EDITION #1
         </h1>
         <p className="bg-gradient-to-r from-blue-200 to-cyan-200 bg-clip-text text-transparent text-center text-[10px]" style={{ fontFamily: 'Amenti Bold' }}>
-          {isMobile() ? 'Swipe to flip the pages' : 'Click to flip the pages'}
+          {isMobile ? 'Touch and hold to flip the pages' : 'Click to flip the pages'}
         </p>
         <div className="bg-white md:mx-16 rounded-[60px] pt-8 relative flex items-center justify-center flex-column">
           <HTMLFlipBook
-            width={isMobile() ? 350 : 500}
-            height={isMobile() ? 550 : 750}
+            width={isMobile ? 350 : 500}
+            height={isMobile ? 550 : 750}
             size="stretch"
-            minWidth={isMobile() ? 350 : 500}
+            minWidth={isMobile ? 350 : 500}
             maxWidth={1000}
-            minHeight={isMobile() ? 600 : 650}
+            minHeight={isMobile ? 600 : 650}
             maxHeight={1350}
             maxShadowOpacity={0.5}
             showCover={false}
-            mobileScrollSupport={false}
+            mobileScrollSupport={true}
             onFlip={(e) => setCurrentPage(e.data)}
             ref={pageFlipRef}
             style={{ margin: 'auto' }}
@@ -47,18 +48,18 @@ const MyBook: React.FC<MyBookProps> = () => {
             startPage={currentPage}
             className=""
             drawShadow={true}
-            usePortrait={isMobile() ? true : false}
+            usePortrait={isMobile ? true : false}
             startZIndex={0}
             autoSize={false}
             clickEventForward={true}
             useMouseEvents={true}
-            swipeDistance={0}
+            swipeDistance={isMobile ? 0 : 10}
             showPageCorners={true}
             disableFlipByClick={false}
           >
             {images.map((pic, index) => (
               <div key={index}>
-                <Image src={pic} alt="" width={isMobile() ? 350 : 500} height={isMobile() ? 600 : 650} quality={100} />
+                <Image src={pic} alt="" width={isMobile ? 350 : 500} height={isMobile ? 600 : 650} quality={100} />
               </div>
             ))}
           </HTMLFlipBook>
