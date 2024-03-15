552fa8e39c15a3ba9e7359631856a6582129cbf7
NirukshaSandeepa <121809388+NirukshaSandeepa@users.noreply.github.com>
2024-03-15 22:17:16 +0530

    feat(86envt1yj): prusted partnerships section basics

---
+++ app/(home)/trustedPartnersSection/trustedPartnersSection.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Image from 'next/image';
+import { useEffect } from 'react';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const imagePaths = [
+  './trustedPartnersImages/IFS.png',
+
+];
+
+const ChaptersSection = () => {
+  const images = imagePaths.map((imagePath) => imagePath);
+
+  useEffect(() => {
+    animateElement('.chaptertitle', { scale: 0.2, opacity: 0 }, { scale: 1, opacity: 1 });
+    animateElement('.logos', { scale:0.2, opacity: 0 }, {scale: 1, opacity: 1, duration: 1 });
+  });
+
+  const animateElement = (selector: string, from: gsap.TweenVars, to: gsap.TweenVars) => {
+    const element = document.querySelectorAll(selector);
+    gsap.fromTo(element, { ...from }, { ...to, duration: 15, ease: 'power.in', stagger: 100, scrollTrigger: getScrollTrigger(selector) });
+  };
+
+  const getScrollTrigger = (trigger: string) => ({
+    trigger,
+    start: 'top bottom',
+    end: 'center center',
+    toggleActions: 'start pause resume pause',
+    scrub: 1,
+  });
+
+  return (
+    <div className="mx-26 mt-[40px] bg-gradient-to-r from-[#07060D] via-[#0E559D] to-[#0E559D] w-screen max-w-[1400px] rounded-[60px] py-5 relative mb-[40px]">
+      <h1 className="chaptertitle my-1 md:text-[40px] text-[30px] text-center mb-4 bg-gradient-to-r from-blue-200 to-cyan-200 bg-clip-text text-transparent" style={{ fontFamily: 'Amenti Bold' }}>TRUSTED PARTNESHIPS</h1>
+      <div className='chapterinner bg-white md:mx-16 mx-7 rounded-[60px] p-10'>
+        <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 md:gap-10 gap-6">
+          {images.map((imagePath, index) => (
+            <div className="logos" key={index} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <Image
+                src={imagePath}
+                alt={`Image from ${imagePath}`}
+                width={250}
+                height={250}
+                objectFit="cover"
+                className="rounded-lg"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChaptersSection;
